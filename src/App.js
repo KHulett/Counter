4a27fba1f10ca55858d7ff0d3ae8f0ac07a079f6
K Hulett <kblocent@gmail.com>
2019-07-05 17:50:54 -0400

    clear button added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,33 @@ class Counter extends Component {
   constructor () {
     super();
     this.state = {
-      counter: 0,
+      counter: 0
     }
   }
 
+
+increment = () => {
+  //This is where we're going to implement our state change!
+  if (this.state.counter < 20) {
+  this.setState({    
+     counter: this.state.counter +1
+    });
+  }
+}
+
+decrement = () => {
+  if (this.state.counter > 0) {
+    this.setState({
+      counter: this.state.counter -1
+    });
+  }
+}
+
+clear = () => {
+  this.setState({
+    counter: 0
+  });
+}
 
   render() {
     console.log("This is line 13 representating state", this.state.counter);
@@ -15,9 +38,10 @@ class Counter extends Component {
       <div className="container">
         <div className="navbar">Counter.js</div>
         <div className="counter">
-          <h1>What do we put here??? :)</h1>
-          <button type="button">Increment</button>
-          <button type="button">Decrement</button>
+          <h1>{this.state.counter}</h1>
+          <button type="button" onClick={this.increment}>Increment</button>
+          <button type="button" onClick={this.decrement}>Decrement</button>
+          <button type="button" onClick={this.clear}>Clear</button>
         </div>
       </div>
     )
