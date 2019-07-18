06eea82c39ac823270ef805f82f675c2f9cc24bf
K Hulett <kblocent@gmail.com>
2019-07-18 14:33:11 -0400

    Updates

---
+++ precache-manifest.44c874e28921e8135a130fde6f1f58b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a0da7d5644815906c70c4d7bbe4b3ec",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "e34c5bcd129380243dfa",
+    "url": "/Counter/static/css/main.016d99f0.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Counter/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "e34c5bcd129380243dfa",
+    "url": "/Counter/static/js/main.d2dd4992.chunk.js"
+  },
+  {
+    "revision": "cd6e87301d7e441c1fae",
+    "url": "/Counter/static/js/runtime~main.130d950d.js"
+  }
+]);
